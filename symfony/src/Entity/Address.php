53843766c0a803fd6a970db8cbbc153fde6e9227
Myrrhe <myrrhe@gmail.com>
2023-02-17 12:45:02 +0100

    feat: Formatting.

--- symfony/src/Entity/Address.php
+++ symfony/src/Entity/Address.php
@@ -14,16 +14,16 @@ class Address
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
-    
+
     #[ORM\Column(length: 255)]
     private ?string $street = null;
-    
+
     #[ORM\Column(length: 255)]
     private ?string $city = null;
-    
+
     #[ORM\Column]
     private ?int $zipCode = null;
-    
+
     #[ORM\Column(length: 255)]
     private ?string $country = null;
 
