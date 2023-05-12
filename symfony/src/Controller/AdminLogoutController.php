140af2285c92fb682f231b613047b32483287213
Myrrhe <myrrhe@gmail.com>
2023-05-12 13:01:11 +0200

    feat: Added a logout, admin home pafe, and redirections.

---
+++ symfony/src/Controller/AdminLogoutController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminLogoutController extends AbstractController
+{
+    #[Route('/admin/logout', name: 'app_admin_logout')]
+    public function index(): Response
+    {
+        return $this->render('admin_logout/index.html.twig', [
+            'controller_name' => 'AdminLogoutController',
+        ]);
+    }
+}
--- symfony/src/Controller/SecurityController.php
+++ symfony/src/Controller/SecurityController.php
@@ -3,10 +3,31 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    #[Route('/admin/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        // To prevent a logged user to come back to the login form
+        if ($this->container->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('app_admin_home');
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+ 
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+    }
+
     #[Route('/logout', name: 'app_logout', methods: ['GET'])]
     public function logout()
     {
